89a1190db9593920631265d84fc6097dfac893bb
Jefferson Silva <jeffsilva1.dev@gmail.com>
2020-09-28 22:11:16 -0300

    esqueleto

---
+++ pages/api/email.js
@@ -0,0 +1,27 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const nodemailer = require('nodemailer')
+
+export default (req, res) => {
+  const { email, name, texto, subject } = req.body
+  
+  const user = "jeffsilva01.dev@gmail.com"
+  const pass = "@Seagatebb01"
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com", //servidor de envio 
+    port: 587, // porta do servidor de envio
+    auth: { user, pass }
+  })
+
+  transporter.sendMail({
+    from: user,
+    to: user,
+    replyTo: email,
+    subject,
+    text: `nome: ${name} \n email: ${email} \n ${texto}`
+  }).then(info => {
+    res.send(info)
+  }).catch(error => {
+    res.send(error)
+  })
+}
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,47 @@
+import { useCallback, useRef } from 'react'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import axios from 'axios'
 
 export default function Home() {
+
+  const refName = useRef()  
+  const refEmail = useRef()  
+  const refAssunto = useRef()  
+  const refTexto = useRef()  
+
+  function handlerSubmit(event) {
+    event.preventDefault()
+
+    console.log("aqui")
+
+     const name = refName.current.value
+     const email = refEmail.current.value
+     const subject = refAssunto.current.value
+     const texto = refTexto.current.value
+
+     axios.post('/api/email', {
+      name,
+      email,
+      subject,
+      texto
+     })
+  }
+
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Envio de Email</title>
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <for className="container">
+        <h1>DE SUA OPINIÃO</h1>
+        <input ref={refName} type="text" placeholder="nome"/>
+        <input ref={refEmail} type="text" placeholder="email"/>
+        <input ref={refAssunto} type="text" placeholder="assunto"/>
+        <input ref={refTexto} type="text" placeholder="texto"/>
+        <button type="button" onClick={handlerSubmit}>enviar</button>
+      </for>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
